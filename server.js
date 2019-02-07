49df6f79cbcfe1f0cf6fc79e80b6051473684e10
veronicorn <vronicalee@gmail.com>
2019-02-06 21:59:29 -0500

    Fixes server.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const path = require('path');
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
